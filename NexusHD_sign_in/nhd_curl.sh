09542d4244c5497f5f367e8d0b1c99e670977f77
Shaw Haines <haohaistone@qq.com>
2022-03-24 11:10:53 +0800

    Changed the sign in message to a more creative one.

--- NexusHD_sign_in/nhd_curl.sh
+++ NexusHD_sign_in/nhd_curl.sh
@@ -6,7 +6,7 @@ password=$2
 cookies="${username}_cookies.txt"
 # Login with username and password, sets auto jump, stores cookies in cookies file.
 curl -d "username=${username}&password=${password}&logout=yes" -L -c $cookies http://www.nexushd.org/takelogin.php -e http://www.nexushd.org/login.php
-# send message [em04] to sign in.
-curl -b $cookies --data-urlencode "action=post" --data-urlencode "content=[em04]" 'http://www.nexushd.org/signin.php'
+# send message [em176][em52] to sign in.
+curl -b $cookies --data-urlencode "action=post" --data-urlencode "content=[em176][em52]" http://www.nexushd.org/signin.php
 # log
 echo "NHD signed in for ${username} at" $(date "+%Y-%m-%d %H:%M:%S") >> log.txt
