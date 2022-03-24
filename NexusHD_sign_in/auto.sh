bf2c6385f33373c07ed2cbe8aec8623232f8fa3c
Shaw Haines <haohaistone@qq.com>
2022-03-24 10:58:17 +0800

    NHD auto sign in script.

---
+++ NexusHD_sign_in/auto.sh
@@ -0,0 +1,13 @@
+# auto.sh
+# Reads username and password from a list
+# and sign in for NexusHD.
+# required password file format:
+# in each line, username:password (separated by colon and nothing else)
+# ended with an empty line.
+pwfile="nhd_password.txt"
+while IFS=: read -r username password
+do
+    # printf 'Username: %s password: %s\n' "$username" "$password"
+    echo "$username" "$password"
+    ./nhd_curl.sh $username $password
+done <"$pwfile"
---
+++ NexusHD_sign_in/nhd_curl.sh
@@ -0,0 +1,12 @@
+# nhd_curl.sh
+# Automatically helps you sign in to NexusHD with your given username and password
+# usage: nhd_curl.sh username password
+username=$1
+password=$2
+cookies="${username}_cookies.txt"
+# Login with username and password, sets auto jump, stores cookies in cookies file.
+curl -d "username=${username}&password=${password}&logout=yes" -L -c $cookies http://www.nexushd.org/takelogin.php -e http://www.nexushd.org/login.php
+# send message [em04] to sign in.
+curl -b $cookies --data-urlencode "action=post" --data-urlencode "content=[em04]" 'http://www.nexushd.org/signin.php'
+# log
+echo "NHD signed in for ${username} at" $(date "+%Y-%m-%d %H:%M:%S") >> log.txt
